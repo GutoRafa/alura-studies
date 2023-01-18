c3e431daebf937ac2b82dfbf4877754f3ecd7b89
Guto <augustorafaelcorrea@gmail.com>
2023-01-18 17:47:03 -0300

    tempo max 10 horas

--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -45,7 +45,7 @@ class Formulario extends React.Component<{
                     onChange={ evento => this.setState({...this.state, tempo: evento.target.value})}
                     id="tempo"
                     min="00:00:00"
-                    max="01:30:00"
+                    max="10:00:00"
                     required
                     />
                 </div>
