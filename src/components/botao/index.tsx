ac8bdeb05b3477549723db0bdb5786c71ad0d9e0
Guto <augustorafaelcorrea@gmail.com>
2023-02-13 14:37:19 -0300

    projeto final

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import style from './botao.module.scss'
 
-class Botao extends React.Component<{type?: "button" | "submit" | "reset" | undefined, children?: React.ReactNode,onClick?: () => void}>{
-    render() {
-        const { type = "button", onClick } = this.props;
-        return (
-            <button onClick={onClick} type={type} className={style.botao}>{this.props.children}</button>
-        )
-    }
+interface Props {
+    type?: "button" | "submit" | "reset" | undefined, children?: React.ReactNode, onClick?: () => void
+}
+
+function Botao ({onClick, type, children}: Props) {
+    return (
+            <button 
+            onClick={onClick} 
+            type={type} 
+            className={style.botao}>
+            {children}
+            </button>
+            )  
 }
 
 export default Botao;
